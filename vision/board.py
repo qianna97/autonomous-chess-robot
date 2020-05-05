577c49acdbf485c7c17dc9a7a772bcc116e6b252
M Imam Muttaqin <imutt20041998@gmail.com>
2020-05-05 19:25:26 +0700

    add kmeans method

--- vision/board.py
+++ vision/board.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 from pprint import pprint
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans, DBSCAN
+from sklearn.preprocessing import normalize
 
 def nothing(x):
     pass
@@ -31,9 +33,9 @@ def intersection(l1,l2):
 
 def clean_line(lines, degree, gaps=1):
     lines = np.array(lines)
-    xlines = np.ravel(lines[:, 0, 0])
-    ylines = np.ravel(lines[:, 0, 1])
-
+    xlines = np.ravel(lines[:,0])
+    ylines = np.ravel(lines[:,1])
+    print("before cleaning :",len(lines))
     tmpX = []
     tmpY = []
 
@@ -65,86 +67,113 @@ def clean_line(lines, degree, gaps=1):
     for i in range(len(lines)):
         if (i not in tmpX) and (i not in tmpY):
             ret.append(lines[i])
-    
+    print("after cleaning :",len(ret))
     return ret
 
 def draw_line(image, lines):
     for line in lines:
-        cv2.line(image,(line[0][0],line[0][1]),(line[1][0],line[1][1]),(0,255,0),1)
+        cv2.line(image,(line[0],line[1]),(line[2],line[3]),(0,255,0),2)
     return image
 
-def find_anomalies(random_data):
-    from sklearn.cluster import DBSCAN
+def find_anomalies(data_):
+    db = DBSCAN(eps=0.05, min_samples=5).fit(data_)
+    labels = db.labels_
 
-    outlier_detection = DBSCAN(min_samples = 2, eps = 3)
-    clusters = outlier_detection.fit_predict(random_data)
-    list(clusters).count(-1)
+    anomali_ = []
 
-createBar()
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER,30,0.001)
+    for i in range(len(labels)):
+        if labels[i] != -1:
+            anomali_.append(data_[i])
+    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+    n_noise_ = list(labels).count(-1)
 
-while True:
-    lines = []
-    degree = []
+    print('Estimated number of clusters: %d' % n_clusters_)
+    print('Estimated number of noise points: %d' % n_noise_)
+    return anomali_
+#createBar()
+lines = []
+degrees = []
 
-    minedge = getBar("Min Edges", "Edges n Lines")
-    maxedge = getBar("Max Edges", "Edges n Lines")
-    threshold = getBar("threshold", "Edges n Lines")
-    minLineLength = getBar("Min Line Length", "Edges n Lines")
-    maxLineGap = getBar("Max Line Gap", "Edges n Lines")
-    con = getBar("con", "Edges n Lines")
+'''
+minedge = getBar("Min Edges", "Edges n Lines")
+maxedge = getBar("Max Edges", "Edges n Lines")
+threshold = getBar("threshold", "Edges n Lines")
+minLineLength = getBar("Min Line Length", "Edges n Lines")
+maxLineGap = getBar("Max Line Gap", "Edges n Lines")
+con = getBar("con", "Edges n Lines")
+'''
+image = cv2.imread('chessboard/Boards/3.jpg')
+image = cv2.resize(image, (int(image.shape[1]*0.2), int(image.shape[0]*0.2)))
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    image = cv2.imread('chessboard/Boards/3.jpg')
-    image = cv2.resize(image, (int(image.shape[1]*0.2), int(image.shape[0]*0.2)))
+#_, thres = cv2.threshold(gray,127,255,cv2.THRESH_BINARY)
 
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(gray,100,300)
 
-    _, thres = cv2.threshold(gray,127,255,cv2.THRESH_BINARY)
+#dst = cv2.cornerHarris(thres,2,3,0.04)
+#dst = cv2.dilate(dst,None)
+#image[dst>0.01*dst.max()]=[255,0,0]
 
-    edges = cv2.Canny(thres,minedge,maxedge)
+houghs = cv2.HoughLines(edges,1,np.pi/180,80)
 
-    dst = cv2.cornerHarris(thres,2,3,0.04)
-    dst = cv2.dilate(dst,None)
-    image[dst>0.01*dst.max()]=[255,0,0]
+data = []
 
-    houghs = cv2.HoughLines(edges,1,np.pi/180,threshold)
-    
-    if houghs is not None:
-        for hough in houghs:
-            rho, theta = hough[0]
-            a = np.cos(theta)
-            b = np.sin(theta)
-            x0 = a*rho
-            y0 = b*rho
-            x1 = int(x0 + con*(-b))
-            y1 = int(y0 + con*(a))
-            x2 = int(x0 - con*(-b))
-            y2 = int(y0 - con*(a))
-            
-            degree.append(int(theta*180/np.pi))
+if houghs is not None:
+    for hough in houghs:
+        rho, theta = hough[0]
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a*rho
+        y0 = b*rho
+        x1 = int(x0 + 1000*(-b))
+        y1 = int(y0 + 1000*(a))
+        x2 = int(x0 - 1000*(-b))
+        y2 = int(y0 - 1000*(a))
+        
+        degree = int(theta*180/np.pi)
+        data.append([degree,rho])
+        #if theta*180/np.pi > 90:
+        #cv2.line(image,(x1,y1),(x2,y2),(0,255,0),1)
+        degrees.append(degree)
+        lines.append([x1,y1,x2,y2])
 
-            #if theta*180/np.pi > 90:
-            cv2.line(image,(x1,y1),(x2,y2),(0,255,0),1)
-            
-            #lines.append([[x1,y1],[x2,y2]])
+    #image = draw_line(image, clean_line(lines, degree, 2))
+    '''
+    pair = []
+    for i in range(len(lines)):
+        for p in range(len(lines)):
+            if lines[i] != lines[p]:
+                if [i,p] not in pair or [p,i] not in pair:
+                    px,py = intersection(lines[i], lines[p])
+                    cv2.circle(image, (px,py), 1, (255,0,0), 2)
+    '''
 
-        #image = draw_line(image, clean_line(lines, degree, 2))
-        '''
-        pair = []
-        for i in range(len(lines)):
-            for p in range(len(lines)):
-                if lines[i] != lines[p]:
-                    if [i,p] not in pair or [p,i] not in pair:
-                        px,py = intersection(lines[i], lines[p])
-                        cv2.circle(image, (px,py), 1, (255,0,0), 2)
-        '''
+#cv2.imshow('thres', thres)
+#cv2.imshow('edges', edges)
+#cv2.imshow('image', image)
 
-    cv2.imshow('thres', thres)
-    cv2.imshow('edges', edges)
-    cv2.imshow('image', image)
 
-    if cv2.waitKey(22) & 0xFF == ord('q'):
-        print(sorted(degree))
-        break
 
-cv2.destroyAllWindows()
+#data = clean_line(lines,degrees,2)
+#data = normalize(data, norm='l2', axis=0)
+#tes = find_anomalies(np.array(data)[:,0])
+#print(tes)
+#image = draw_line(image, data)
+
+
+
+#kmeans = KMeans(n_clusters=2, random_state=0).fit(data)
+#print(len(kmeans.labels_))
+
+plt.figure(1)
+plt.scatter(np.arange(0,len(degrees)), np.array(lines)[:,0])
+plt.figure(2)
+plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+plt.show()
+
+'''
+if cv2.waitKey(22) & 0xFF == ord('q'):
+    print(sorted(degree))
+
+    cv2.destroyAllWindows()
+'''
--- vision/tes.py
+++ vision/tes.py
@@ -1,13 +1,30 @@
 import numpy as np
 import cv2 as cv
+from matplotlib import pyplot as plt
 #filename = 'chessboard/Boards/coba.png'
 filename = 'chessboard/Boards/6_edited.jpg'
 img = cv.imread(filename)
 gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-gray = np.float32(gray)
-dst = cv.cornerHarris(gray,2,3,0.04)
-dst = cv.dilate(dst,None)
-img[dst>0.01*dst.max()]=[0,255,0]
+
+edges = cv.Canny(gray,100,300)
+
+houghs = cv.HoughLines(edges,1,np.pi/180,100)
+
+hx = []
+hy = []
+
+for hough in houghs:
+    rho, theta = hough[0]
+    hx.append(int(theta*180/np.pi))
+    hy.append(rho)
+
+
+plt.scatter(hx,hy)
+plt.show()
+
+'''
 cv.imshow('dst',img)
+
 if cv.waitKey(0) & 0xff == 27:
     cv.destroyAllWindows()
+'''
